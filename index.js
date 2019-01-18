013bb365c20806d603daa283ea32d2338207bdc9
Ruud Verheijden <ruud.verheijden@gmail.com>
2019-01-18 11:06:29 +0100

    Refactoring cache to array instead of file and adding a retry mechanism to resend undelivered data

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const P1Reader = require('p1-reader');
 const Influx = require('influx');
-const fs = require('fs');
 const config = require('./config.json');
 
-const undeliveredDataFile = 'undeliveredData.csv';
+let cache = [];
+const cacheMaxLength = 10000; // To prevent memory issues when InfluxDB is not available for a long time
+const cacheRetryBatchSize = 300; // Size of the batch to retry sending data to InfluxDB
+const influxMeasurement = 'p1-readings';
 
 // Instantiate P1 Reader en InfluxDB
-const p1Reader = new P1Reader();
+const p1Reader = new P1Reader({debug: true});
 const influx = new Influx.InfluxDB({
     database: config.database,
     host: config.host,
@@ -16,7 +18,7 @@ const influx = new Influx.InfluxDB({
     password: config.password,
     schema: [
       {
-        measurement: 'p1-readings',
+        measurement: influxMeasurement,
         fields: {
             electricity_tarrif1: Influx.FieldType.FLOAT,
             electricity_tarrif2: Influx.FieldType.FLOAT,
@@ -36,36 +38,17 @@ p1Reader.on('connected', portConfig => {
 });
 
 p1Reader.on('reading', data => {
-    // Write P1 readings to the InfluxDB server
-    influx.writePoints([
-        {
-            measurement: 'p1-readings',
-            tags: {},
-            fields: { 
-                electricity_tarrif1: data.electricity.received.tariff1.reading,
-                electricity_tarrif2: data.electricity.received.tariff2.reading,
-                electricity_actual: data.electricity.received.actual.reading,
-                gas_reading: data.gas.reading
-            },
-            timestamp: new Date(data.timestamp),
-        }
-        ], {
-            precision: 's',
-        })
-        .catch(error => {
-            // Cache the data and try again later since we could not store it right now
-            cacheUndeliveredData(
-                data.timestamp,
-                data.electricity.received.tariff1.reading,
-                data.electricity.received.tariff2.reading,
-                data.electricity.received.actual.reading,
-                data.gas.reading
-            );
-        });
+    writeToInfluxDB([{
+        timestamp: data.timestamp,
+        electricity_tarrif1: data.electricity.received.tariff1.reading,
+        electricity_tarrif2: data.electricity.received.tariff2.reading,
+        electricity_actual: data.electricity.received.actual.reading,
+        gas_reading: data.gas.reading
+    }]);
 });
 
 p1Reader.on('error', error => {
-    console.log(error);
+    console.log('ERROR:', error);
 });
 
 p1Reader.on('close', () => {
@@ -77,16 +60,68 @@ process.on('uncaughtException', error => {
     console.error(error);
 });
 
-// Write data to CSV file if it could not be delivered to the InfluxDB server to be processed later
-function cacheUndeliveredData (timestamp, electricity_tarrif1, electricity_tarrif2, electricity_actual, gas_reading) {
-    var csvOutput = '' +
-    timestamp + ';' +
-    electricity_tarrif1 + ';' +
-    electricity_tarrif2 + ';' +
-    electricity_actual + ';' +
-    gas_reading + '\n';
+// Retry to process a batch of undelivered data every 5 minutes
+setInterval(() => {
+    if (cache.length > 0) {
+        console.log('Retrying sending a batch of previously undelivered data to InfluxDB');
 
-    fs.appendFile(undeliveredDataFile, csvOutput, error => {
-        console.log('Could not write undelivered data to file', error);
+        let retryDatapoints = [];
+    
+        // Get a batch of cached items to be send all at once
+        for(let i = 1; i <= cacheRetryBatchSize; i++) {
+            retryDatapoints.push(cache.shift());
+    
+            if (cache.length == 0) {
+                break;
+            }
+        }
+    
+        writeToInfluxDB(retryDatapoints);
+    }
+}, 1 * 60 * 1000);
+
+// Write an array of data points to InfluxDB
+function writeToInfluxDB (dataPoints) {
+    let influxPoints = [];
+
+    for (let i = 0; i < dataPoints.length; i++) {
+        influxPoints.push({
+            measurement: influxMeasurement,
+            tags: {},
+            fields: { 
+                electricity_tarrif1: dataPoints[i].electricity_tarrif1,
+                electricity_tarrif2: dataPoints[i].electricity_tarrif2,
+                electricity_actual: dataPoints[i].electricity_actual,
+                gas_reading: dataPoints[i].gas_reading
+            },
+            timestamp: new Date(dataPoints[i].timestamp)
+        });
+    }
+
+    // Write P1 readings to the InfluxDB server
+    influx.writePoints(influxPoints, { precision: 's' })
+    .catch(error => {
+        // Cache the data and try again later since we could not store it right now
+        cacheUndeliveredData(dataPoints);
     });
+}
+
+// Cache data that could not be delivered to the InfluxDB server to be processed later
+function cacheUndeliveredData (dataPoints) {
+    while (dataPoints.length > 0) {
+        let point = dataPoints.shift();
+
+        cache.push({
+            timestamp: point.timestamp,
+            electricity_tarrif1: point.electricity_tarrif1,
+            electricity_tarrif2: point.electricity_tarrif2,
+            electricity_actual: point.electricity_actual,
+            gas_reading: point.gas_reading
+        });
+
+        // Remove the oldest item if we reached the cache max length to prevent memory issues
+        if (cache.length > cacheMaxLength) {
+            cache.shift();
+        }
+    }
 }
